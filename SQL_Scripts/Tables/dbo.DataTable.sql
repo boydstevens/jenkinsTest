737739cf39c50a941eb36e1fa1d964c59c5ed344
boydstevens <boyd.stevens@gmail.com>
2015-08-13 09:43:29 -0700

    DataTable now supports names up to 100 characters, from 50

--- SQL_Scripts/Tables/dbo.DataTable.sql
+++ SQL_Scripts/Tables/dbo.DataTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[DataTable]
 (
 [Some_PK] [int] NOT NULL IDENTITY(1, 1),
-[DataName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DataName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [DataValue] [int] NOT NULL
 ) ON [PRIMARY]
 GO
