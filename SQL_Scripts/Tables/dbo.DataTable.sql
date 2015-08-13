c63a4a197e4ca5b10550422120d76a45ef43dadf
boydstevens <boyd.stevens@gmail.com>
2015-08-13 08:39:03 -0700

    Created DataTable for storate of data

---
+++ SQL_Scripts/Tables/dbo.DataTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[DataTable]
+(
+[Some_PK] [int] NOT NULL IDENTITY(1, 1),
+[DataName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[DataValue] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[DataTable] ADD CONSTRAINT [PK_DataTable] PRIMARY KEY CLUSTERED  ([Some_PK]) ON [PRIMARY]
+GO
