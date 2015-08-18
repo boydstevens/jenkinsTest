4abbbf2d24ec32fa8b0bbedb3ef403e7a4f34d94
boydstevens <boyd.stevens@gmail.com>
2015-08-18 13:34:35 -0400

    Example Change

--- SQL_Scripts/Tables/dbo.DataTable.sql
+++ SQL_Scripts/Tables/dbo.DataTable.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[DataTable]
 (
 [Some_PK] [int] NOT NULL IDENTITY(1, 1),
 [DataName] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[DataValue] [int] NOT NULL
+[DataValue] [int] NOT NULL,
+[SomeCol] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[DataTable] ADD CONSTRAINT [PK_DataTable] PRIMARY KEY CLUSTERED  ([Some_PK]) ON [PRIMARY]
